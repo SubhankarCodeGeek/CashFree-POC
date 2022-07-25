683592f4baef23617a121ffd656e6934155ced61
Subhankar Bag <subhankar.bag@codelogicx.com>
2022-07-25 19:00:51 +0530

    ~ hilt and kotlin version compatibility with compose compiler related issue is fixed.

---
+++ app/src/main/java/com/example/cashfree_poc/CashFreePaymentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cashfree_poc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CashFreePaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cash_free_payment)
+    }
+}
--- app/src/main/java/com/example/cashfree_poc/MainActivity.kt
+++ app/src/main/java/com/example/cashfree_poc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.cashfree_poc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -12,10 +13,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cashfree_poc.ui.home.HomeScreen
 import com.example.cashfree_poc.ui.theme.Cashfree_pocTheme
+import com.example.cashfree_poc.ui.viewModel.PaymentViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel : PaymentViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
